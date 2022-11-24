7ff2f701181c2211cc0319b6ec03653cfd2869f5
csilfverskiold <christian.silfverskiold@gmail.com>
2022-11-24 11:58:50 +0000

    Set up structure of javascript functions

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+/**
+ * This function displays the players choice in the game
+ */
+ function playerChoice() {
+
+}
+
+/**
+ * This function randomly generates and displays the
+ * computers choice in the game
+ */
+function generateComputerChoice() {
+
+}
+
+/**
+ * This function displays the players current winning score
+ */
+function incrementWin() {
+
+}
+
+/**
+ * This function displays the players current losing score
+ */
+function incrementLoss() {
+
+} 
+
+/**
+ * This function displays the amount of rounds played 
+ */
+function incrementRounds() {
+    
+}
