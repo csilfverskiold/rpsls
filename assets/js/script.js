b07ca6778abff260c8e7e25821bbd8c69a3d43be
csilfverskiold <christian.silfverskiold@gmail.com>
2022-11-24 17:45:18 +0000

    Add function to randomly generate computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,26 +8,35 @@ const playerChoiceDisplay = document.getElementById('player-choice')
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const resultDisplay = document.getElementById('result')
 const buttonChoices = document.querySelectorAll('button')
-let userChoice
+let computerChoice
 
 buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener('click', (e) => {
-userChoice = e.target.id 
-playerChoiceDisplay.innerHTML = userChoice
+playerChoice = e.target.id 
+playerChoiceDisplay.innerHTML = playerChoice
+generateComputerChoice()
+
 })) // The block of code above was slightly reworded for my project and taken from: https://www.youtube.com/watch?v=RwFeg0cEZvQ 
 
-
-/**
- * This function displays the players choice in the game
- */
- function playerChoice() {
-
-}
 
 /**
  * This function randomly generates and displays the
  * computers choice in the game
  */
 function generateComputerChoice() {
+
+    let generateNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (generateNumber == 1) {
+        computerChoice = "rock"
+    }
+    if (generateNumber == 2) {
+        computerChoice = "paper"
+    }
+    if (generateNumber == 3) {
+        computerChoice = "scissors"
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
 
 }
 
